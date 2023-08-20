aca86bdc24f4c8b6311edc376bb9d1df85fc4334
Fahim <afahim1110@gmail.com>
2023-08-20 20:55:33 +0100

    Implement parallel execution

--- run_experiments.py
+++ run_experiments.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import concurrent.futures
 
 # List of input folders
 input_folders = [
@@ -17,26 +18,32 @@ output_parent_folder = "outputs"
 # Set the timeout
 TIMEOUT = 10
 
-# Loop through input folders and algorithms
-for input_folder in input_folders:
-    for algorithm in algorithms:
-        # Create an output folder for each combination of input folder and algorithm
-        output_folder = os.path.join(output_parent_folder, os.path.basename(input_folder), algorithm)
-        os.makedirs(output_folder, exist_ok=True)
+def process_folder(input_folder, algorithm):
+    # Create an output folder for each combination of input folder and algorithm
+    output_folder = os.path.join(output_parent_folder, os.path.basename(input_folder), algorithm)
+    os.makedirs(output_folder, exist_ok=True)
+    
+    # Get a list of .spectra files in the input folder
+    spectra_files = [file for file in os.listdir(input_folder) if file.endswith(".spectra")]
+    
+    # Loop through the .spectra files and run spec_repair.py for each
+    for spectra_file in spectra_files:
+        input_file = os.path.join(input_folder, spectra_file)
         
-        # Get a list of .spectra files in the input folder
-        spectra_files = [file for file in os.listdir(input_folder) if file.endswith(".spectra")]
-        
-        # Loop through the .spectra files and run spec_repair.py for each
-        for spectra_file in spectra_files:
-            input_file = os.path.join(input_folder, spectra_file)
-            
-            # Construct the command to run spec_repair.py
-            command = f"python spec_repair.py -i {input_file} -a {algorithm} -o {output_folder} -t {TIMEOUT}"
+        # Construct the command to run spec_repair.py
+        command = f"python spec_repair.py -i {input_file} -a {algorithm} -o {output_folder} -t {TIMEOUT}"
 
-            print(f"Repairing {spectra_file} using {algorithm} algorithm in {input_folder}...")
-            
-            try:
-                subprocess.run(command, shell=True, check=True)
-            except subprocess.CalledProcessError:
-                print(f"Error executing spec_repair.py for {spectra_file}")
+        print(f"Repairing {spectra_file} using {algorithm} algorithm in {input_folder}...")
+        
+        try:
+            subprocess.run(command, shell=True, check=True)
+        except subprocess.CalledProcessError:
+            print(f"Error executing spec_repair.py for {spectra_file}")
+
+# Use ThreadPoolExecutor for parallel processing
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    for input_folder in input_folders:
+        for algorithm in algorithms:
+            executor.submit(process_folder, input_folder, algorithm)
+
+print("All tasks completed.")
