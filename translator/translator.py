afca1a56a7cf6d4cb18d7abbc29d7215f28411b2
Fahim <afahim1110@gmail.com>
2023-08-26 20:27:36 +0100

    Refactor translator and re-translate specs

--- translator/translator.py
+++ translator/translator.py
@@ -1,26 +1,18 @@
-import sys,traceback
+import sys
+import os
+import re
+import subprocess
 from pyparsing import *
 from pyparsing import pyparsing_common
 ParserElement.enablePackrat()
 
-import xml.etree.ElementTree as ET
-import re
-import os
-import subprocess
-
-sys.setrecursionlimit(1500)
-
-####################################### XML Init ##############################################
-# root = ET.Element("project")
-# signals = ET.SubElement(root, "signals")
-# requirements = ET.SubElement(root, "requirements")
-
-####################################### Spec Init ##############################################
-
-spec = []
+env_variables = dict()
+sys_variables = dict()
 
 ####################################### Parse classes #########################################
+
 class Variable:
+
     def __init__(self, name, boolean, values=None):
         self.name = name
         self.boolean = boolean # True: Boolean  False: enum
@@ -52,44 +44,10 @@ class Variable:
         else:
             return "(" + " | ".join(literals) + ")"
 
-    def convertInequalityToBooleanFormula(self, operator, value, next=False):
-        # First convert the constant name if value is not an integer
-        if value in integer_consts:
-            value = integer_consts[value]
-
-        # Then check the bounds on the variable and convert the inequality operator into a disjunction of equalities
-        # over the Boolean variables
-        equalities = []
-        if operator == "<=" or operator == "<":
-            for i in range(0, min([len(self.values),value])):
-                equalities.append(self.convertFromEnumLabelToBooleanFormula(i,False,next))
-        elif operator == ">=" or operator == ">":
-            for i in range(max([0,value]), len(self.values)):
-                equalities.append(self.convertFromEnumLabelToBooleanFormula(i, False, next))
-        if operator == "<=" or operator == ">=" and value>=0 and value < len(self.values):
-            equalities.append(self.convertFromEnumLabelToBooleanFormula(i,False,next))
-
-        return "(" + " | ".join(equalities) + ")"
-    
-
-env_variables = dict()
-sys_variables = dict()
-variables = dict()
-
-integer_consts = dict()
-
-assumptions = []
-guarantees = []
 
 ####################################### Parse actions #########################################
 
 def parseDefinition(t):
-    if t[0] != "define":
-        return parseVariable(t)
-    spec.append("define\n\t" + " ".join(t[1:]) + ";")
-
-
-def parseVariable(t):
     kind = t[0]
     boolean = t[1] == "boolean"
     name = t[-2]
@@ -130,29 +88,7 @@ def parseVariable(t):
             safety += "alw (!(" + " & ".join(literals) + "));\n"
             ret += safety
 
-    variables[name] = var_obj
     return ret
-
-
-def parseDefIntegerVariable(t):
-    values = range(int(t[2]),int(t[4])+1)
-    name = t[-1]
-    var_obj = Variable(name, False, values)
-    if t[0] == "env":
-        env_variables[name] = var_obj
-    elif t[0] == "sys":
-        sys_variables[name] = var_obj
-    variables[name] = var_obj
-
-    return var_obj
-
-
-def parseDefIntegerConst(t):
-    integer_consts[t[1]] = int(t[3])
-
-def parseNegation(t):
-    return [[t[0][0], t[0][1:]]]
-
 
 def parseComparison(t):
     # This is very complex. Comparisons can be:
@@ -209,7 +145,7 @@ def parseComparison(t):
             else:
                 return "(" + " | ".join(formula_bits) + ")"
 
-    elif len(t) == 4:
+    else:
         # There is some next operator involved
         # Previous ("Y") operators are dealt with in the relevant parser
         token_list = t._ParseResults__toklist
@@ -256,185 +192,16 @@ def parseComparison(t):
             return "(" + " | ".join(formula_bits) + ")"
         else:
             return " & ".join(formula_bits)
-    else:
 
-        token_list = t._ParseResults__toklist
-        operand1 = t[1][0]
-        operand2 = t[4][0]
-
-        if operand1 in env_variables:
-            var1 = env_variables[operand1]
-        else:
-            var1 = sys_variables[operand1]
-
-        # X(var) =/!= var
-        if var1.boolean:
-            return "next(" + ("!" if "!=" in token_list else "") + operand1 + ")" \
-                    + " <-> " \
-                    + "next(" + ("!" if "!=" in token_list else "") + operand2 + ")"
-
-        formula_bits = []
-        for i in range(var1.num_bits):
-            formula_bits.append("(next(" + operand1 + "_" + str(i) + ")"
-                                + " <-> " \
-                                + "next(" + ("!" if "!=" in token_list else "") + operand2 + "_" + str(i) + "))")
-        if  "!=" in token_list:
-            return "(" + " | ".join(formula_bits) + ")"
-        else:
-            return " & ".join(formula_bits)
-
-
-def parseInequality(t):
-
-    # Transform the inequality (for instance v<5) into a set of disjoint equalities (v=0 | v=1 | ...)
-    var_obj = variables[t[0][0]]
-
-
-
-def parseNext(t):
-    return ["X", [t[0][1]]]
-
-def parsePrev(t):
-    return ["PREV", t[0][1:]]
-
-identifier_pattern = re.compile(r"\w+")
-# count_prevs = 0
-# def parsePrev(t):
-#     global count_prevs
-
-#     prev_bool_expr = str(t[0][1])
-
-#     # Need to check whether this auxiliary variable is an env or sys variable
-#     # Match the variable name in the expression
-#     # var_name contains the name of the first variable used in the expression.
-#     # We are assuming arguments of PREV use only one variable. They are expressions
-#     # of the kind variable = CONSTANT.
-#     # When parsePrev is called, those expressions have already been parsed by parseComparison,
-#     # hence been transformed into Boolean expressions like variable_0 & !variable_1.
-#     # In order to recover the variable name and search the lists of environment and controller
-#     # variables, we perform this pattern matching and possible remove the final _i
-#     var_name = identifier_pattern.findall(prev_bool_expr)[0]
-
-#     is_environment_variable = False
-#     if var_name in env_variables or var_name.rsplit('_', 1)[0] in env_variables:
-#         is_environment_variable = True
-
-#     # Introduce the auxiliary variable yboolexpr_i
-#     fresh_variable_name = "yBoolExpr_" + str(count_prevs)
-#     if is_environment_variable:
-#         env_variables[fresh_variable_name] = Variable(fresh_variable_name, True)
-#     else:
-#         sys_variables[fresh_variable_name] = Variable(fresh_variable_name, True)
-#     count_prevs += 1
-
-#     # Add constraint binding this fresh variable to the Boolean expression
-#     if is_environment_variable:
-#         assumptions.append("G((" + prev_bool_expr + ") <-> X(" + fresh_variable_name + "))")
-#     else:
-#         guarantees.append("G((" + prev_bool_expr + ") <-> X(" + fresh_variable_name + "))")
-
-#     # The parsed formula is transformed into the fresh variable literal
-#     return [fresh_variable_name]
-
-
-# count_responds_to = 0
-def parseRespondsTo(t):
-    trigger = t[1]
-    response = t[2]
-    return ["respondsTo(" + trigger + "," + response + ")"]
-
-    # global count_responds_to
-
-    # trigger = t[1]
-    # response = t[2]
-
-    # # Like in parsePrec, this needs to figure out whether this condition is an environment
-    # # or a controller constraint
-    # var_name = identifier_pattern.findall(response)[0]
-
-    # is_environment_variable = False
-    # if var_name in env_variables or var_name.rsplit('_', 1)[0] in env_variables:
-    #     is_environment_variable = True
-
-    # fresh_variable_name = "responded_" + str(count_responds_to)
-    # responded = Variable(fresh_variable_name, True)
-    # count_responds_to += 1
-
-    # if is_environment_variable:
-    #     env_variables[fresh_variable_name] = responded
-    #     assumptions.append("G(F(" + fresh_variable_name + "))")
-    # else:
-    #     sys_variables[fresh_variable_name] = responded
-    #     guarantees.append("G(F(" + fresh_variable_name + "))")
-
-    # return ["G(X(" + fresh_variable_name + ") <-> ((" + response + ") | !(" + trigger + ")))"]
-
-
-def parseAlwaysEventually(t):
-    return ["GF", [t[0][1]]]
-
-def parseTemporalLogicProperty(t):
-    formula = ""
-    if len(t)==1:
-        if type(t[0]) == str:
-            return t[0]
-        else:
-            return parseTemporalLogicProperty(t[0])
-    for x in t:
-        if type(x) == str:
-            formula = formula + (x if len(formula) == 0 or formula[-1] == "!" else (" " + x))
-        else:
-            formula = formula + "(" + parseTemporalLogicProperty(x) + ")"
-    return formula
-
-def parseConstraint(t):
-    if t[0] == "assumption":
-        assumptions.append(t[1])
-    else:
-        guarantees.append(t[1])
-    spec.append(t[0] + "\n\t" + t[1] + ";")
-
-def parsePattern(t):
-    for items in t:
-        line = "pattern "
-        for item in items:
-            if item.endswith('{') or item.endswith(';'):
-                line += item
-                spec.append(line)
-                line = ""
-            else:
-                line += item + " "
-        spec.append("}")
-
-def printStatement(t):
-    print(t)
 
 ############################### Grammar ##################################################
 
 comment = ("--" + restOfLine) | nestedExpr('/*', '*/') | ("//" + restOfLine)
-
 seq = Literal(";")
-always_op = Keyword("G") | Keyword("alw")
-eventually_op = Keyword("F")
-alwayseventually_op = Keyword("GF") | Keyword("alwEv")
-# next_op = Keyword("X") | Keyword("next")
-prev_op = Keyword("PREV") | Keyword("Y")
-
-neg_op = Literal("!")
-and_op = Literal("&") | Keyword("and")
-or_op = Literal("|") | Keyword("or")
-implies_op = Literal("->")
-iff_op = Literal("<->") | Keyword("iff")
-
 
 neq_op = Literal("!=")
 eq_op = Literal("=")
 comparison_op = neq_op | eq_op
-leq_op = Literal("<=")
-lt_op = Literal("<")
-geq_op = Literal(">=")
-gt_op = Literal(">")
-inequality_op = leq_op | lt_op | geq_op | gt_op
 
 identifier = pyparsing_common.identifier
 enum_literal = Word(alphas.upper(), alphas.upper() + nums + "_")
@@ -442,96 +209,26 @@ enum_literal = Word(alphas.upper(), alphas.upper() + nums + "_")
 next_op = Literal("next") + Suppress("(") + identifier + Suppress(")")
 bool_atom = next_op | identifier | enum_literal
 
-atom = bool_atom | Word(nums)
-
-bool_expr = infixNotation(bool_atom,
-                             [(next_op, 1, opAssoc.RIGHT, parseNext),
-                              (neg_op, 1, opAssoc.RIGHT, parseNegation),
-                              (prev_op, 1, opAssoc.RIGHT, parsePrev),
-                              (comparison_op, 2, opAssoc.LEFT, parseComparison),
-                              (inequality_op, 2, opAssoc.LEFT, parseInequality),
-                              (and_op, 2, opAssoc.LEFT),
-                              (or_op, 2, opAssoc.LEFT),
-                              (implies_op, 2, opAssoc.LEFT),
-                              (iff_op, 2, opAssoc.LEFT)])
-
-bool_expr.setParseAction(parseTemporalLogicProperty)
-
-responds_to = Keyword("respondsTo") + Suppress("(") + bool_expr + Suppress(",") + bool_expr + Suppress(")")
-responds_to.setParseAction(parseRespondsTo)
-
-temporal_logic_expr = responds_to | infixNotation(atom,
-                                    [(neg_op, 1, opAssoc.RIGHT, parseNegation),
-                                     (next_op, 1, opAssoc.RIGHT, parseNext),
-                                     (prev_op, 1, opAssoc.RIGHT, parsePrev),
-                                     (comparison_op, 2, opAssoc.LEFT, parseComparison),
-                                     (inequality_op, 2, opAssoc.LEFT, parseInequality),
-                                     (and_op, 2, opAssoc.LEFT),
-                                     (or_op, 2, opAssoc.LEFT),
-                                     (implies_op, 2, opAssoc.LEFT),
-                                     (iff_op, 2, opAssoc.LEFT),
-                                     (always_op, 1, opAssoc.RIGHT),
-                                     (eventually_op, 1, opAssoc.RIGHT),
-                                     (alwayseventually_op, 1, opAssoc.RIGHT, parseAlwaysEventually)])
-
-temporal_logic_expr.setParseAction(parseTemporalLogicProperty)
-
-def_integer_const = Keyword("define") + identifier + Literal(":=") + Word(nums)
-def_integer_const.setParseAction(parseDefIntegerConst)
-
 def_boolean = (Keyword("env") | Keyword("sys") | Keyword("aux")) + Keyword("boolean") + identifier
 def_enum = (Keyword("env") | Keyword("sys")) \
            + Literal("{") + delimitedList(enum_literal) + Literal("}") \
            + identifier
-def_internal_variable = (Keyword("define") + identifier + Literal(":=") + temporal_logic_expr)
 
-definition = (def_boolean | def_enum | def_internal_variable) + seq
+definition = (def_boolean | def_enum) + seq
 definition.ignore(comment)
 definition.setParseAction(parseDefinition)
-
-def_integer_variable = (Keyword("env") | Keyword("sys")) \
-           + Literal("Int(") + Word(nums) + Literal("..") + Word(nums) + Literal(")") \
-           + identifier
-def_integer_variable.ignore(comment)
-def_integer_variable.setParseAction(parseDefIntegerVariable)
-
-constraint = (Keyword("assumption") | Keyword("guarantee")) + (temporal_logic_expr | bool_expr)
-constraint.ignore(comment)
-constraint.setParseAction(parseConstraint)
-
-statement = (def_integer_const | def_integer_variable | definition | constraint) + seq
-statement.ignore(comment)
-# statement.setParseAction(printStatement)
-
-pattern = nestedExpr("pattern", "}")
-pattern.setParseAction(parsePattern)
-
-program = Keyword("module") + identifier \
-          + OneOrMore(statement) \
-          + ZeroOrMore(pattern)
-
-program.ignore(comment)
 
 comparison_exp = bool_atom + comparison_op + bool_atom
 comparison_exp.setParseAction(parseComparison)
 
-###################################### IO ####################################################
 
-def write_file(spec, output_filename):
-    output = '\n'.join(spec)
-    file = open(output_filename, 'w')
-    file.write(output)
-    file.close()
-
-###################################### Tests ##################################################
-
+###################################### Main ##################################################
 
 input_path = sys.argv[1]
 
 if len(sys.argv) >= 3:
     output_directory = sys.argv[2]
 else:
-    # Default output directory if not provided
     output_directory = "outputs"
 
 if not os.path.isdir(output_directory):
@@ -539,8 +236,6 @@ if not os.path.isdir(output_directory):
 
 directory, filename = os.path.split(input_path)
 print "TRANSLATING: " + filename
-
-# === Run SpecTranslator.java ===
 
 PATH_TO_JAR = "SpecTranslator.jar"
 cmd = "java -jar {} -i {} -o {}".format(PATH_TO_JAR, input_path, output_directory)
@@ -567,17 +262,3 @@ new_spec = re.sub(r"alw\s*\(", r"G (", new_spec)
 
 with open(new_path, "w") as output_file:
     output_file.write(new_spec)
-
-# Normalize syntax
-# spec = [re.sub(r"G\s*\(", r"alw (", line) for line in spec]
-# spec = [re.sub(r"GF\s*\(", r"alwEv (", line) for line in spec]
-# spec = [re.sub(r"alwEv\s*\(", r"GF (", line) for line in spec]
-# spec = [re.sub(r"alw\s*\(", r"G (", line) for line in spec]
-# spec = [re.sub(r"X\s*\(", r"next(", line) for line in spec]
-# spec = [re.sub(r"Y\s*\(", r"PREV(", line) for line in spec]
-# spec = [re.sub(r'(\w+)=true', r'\1', x) for x in spec]
-# spec = [re.sub(r'(\w+)=false', r'!\1', x) for x in spec]
-
-
-
-
