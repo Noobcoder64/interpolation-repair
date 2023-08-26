c54294949feb694a7659ceac4f411cbc398d2ce7
Fahim <afahim1110@gmail.com>
2023-08-26 20:06:13 +0100

    Translate specs

--- translator/translate_multiple.sh
+++ translator/translate_multiple.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
 
-# for filepath in inputs/SIMPLE/*.spectra
-# do
-#         java -jar SpecTranslator.jar -i ${filepath} -o outputs/SIMPLE
-# done
+for filepath in inputs/SIMPLE/*.spectra
+do
+        java -jar SpecTranslator.jar -i ${filepath} -o outputs/SIMPLE
+done
 
-# for filepath in inputs/AMBA/*.spectra
-# do
-#         java -jar SpecTranslator.jar -i ${filepath} -o outputs/AMBA
-# done
-
-# for filepath in inputs/SYNTECH15-UNREAL/*.spectra
-# do
-#         python translator.py ${filepath} outputs/SYNTECH-UNREAL/
-# done
-
-# for filepath in inputs/SYNTECH15-1UNREAL/*.spectra
-# do
-#         python translator.py ${filepath} outputs/SYNTECH15-1UNREAL/
-# done
+for filepath in inputs/AMBA-ORIGINAL/*.spectra
+do
+        java -jar SpecTranslator.jar -i ${filepath} -o outputs/AMBA
+done
 
 for filepath in inputs/SYNTECH15-UNREAL-ORIGINAL/*.spectra
 do
-        python translator.py ${filepath} outputs/BOOL-TRANSLATED-G
+        python translator.py ${filepath} outputs/SYNTECH15-UNREAL/
+done
+
+for filepath in inputs/SYNTECH15-1UNREAL-ORIGINAL/*.spectra
+do
+        python translator.py ${filepath} outputs/SYNTECH15-1UNREAL/
 done
