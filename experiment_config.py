afa6eccddf499174b2fac8bb30300061a27c56e8
Fahim Ahmed <fa1622@ray02.doc.ic.ac.uk>
2023-10-17 20:54:22 +0100

    Merge

--- experiment_config.py
+++ experiment_config.py
@@ -14,25 +14,25 @@ FLAGS = " ".join([
     "-inf",
 ])
 
-# INPUT_PARENT_FOLDER = "inputs/"
+INPUT_PARENT_FOLDER = "inputs/"
 # INPUT_PARENT_FOLDER = "inputs-interpolation/"
-INPUT_PARENT_FOLDER = "inputs-scalability/"
+# INPUT_PARENT_FOLDER = "inputs-scalability/"
 
-
-# INPUT_FOLDERS = [
-#     "SIMPLE",
-#     # "inputs/AMBA-1",
-#     "AMBA-2",
-#     "SYNTECH15-UNREAL",
-#     "SYNTECH15-1UNREAL",
-# ]
 
 INPUT_FOLDERS = [
-    "addedVarsRG1",
-    # "addedVarsLift",
-    # "addedVarsHumanoid458",
-    # "addedVarsGyro_Var1",
+    # "SIMPLE",
+    "AMBA-1",
+    # "AMBA-2",
+    # "SYNTECH15-UNREAL",
+    # "SYNTECH15-1UNREAL",
 ]
+
+# INPUT_FOLDERS = [
+#     "addedVarsRG1",
+#     "addedVarsLift",
+#     "addedVarsHumanoid458",
+#     "addedVarsGyro_Var1",
+# ]
 
 SYSTEMS = [
     "amba",
@@ -44,14 +44,14 @@ SYSTEMS = [
 
 # Output parent folder
 # OUTPUT_PARENT_FOLDER = "outputs/"
-# OUTPUT_PARENT_FOLDER = "outputs-interpolation/"
+OUTPUT_PARENT_FOLDER = "outputs-interpolation/"
 # OUTPUT_PARENT_FOLDER = "outputs-symbolic/"
-OUTPUT_PARENT_FOLDER = "outputs-scalability/"
+# OUTPUT_PARENT_FOLDER = "outputs-scalability/"
 # OUTPUT_PARENT_FOLDER = "outputs-weakness/"
 # OUTPUT_PARENT_FOLDER = "outputs-translation/"
 
-RUN = 1
+RUN = 2
 
 TIMEOUT = 10
 
-REPAIR_LIMIT = 1
+REPAIR_LIMIT = -1
